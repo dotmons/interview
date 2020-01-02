2d24945add16b345c24cb9d5d32cb4120a1d4b33
Adeoye Oludotun <dotmons@gmail.com>
2020-01-02 13:31:50 -0500

    Adding code base: Transport System

--- Interviews/src/amz/TransportSystem.java
+++ Interviews/src/amz/TransportSystem.java
@@ -5,20 +5,66 @@
  */
 package amz;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Dotmons
  */
 public class TransportSystem {
-    
-    public TransportSystem(){
-        
+
+    public TransportSystem() {
+
+        List<List<Integer>> area = new ArrayList<>();
+        List<Integer> listValues = new ArrayList();
+        listValues.add(1);
+        listValues.add(0);
+        listValues.add(0);
+        area.add(listValues);
+
+        listValues = new ArrayList();
+        listValues.add(1);
+        listValues.add(0);
+        listValues.add(0);
+        area.add(listValues);
+
+        listValues = new ArrayList();
+        listValues.add(1);
+        listValues.add(9);
+        listValues.add(1);
+        area.add(listValues);
+
+        System.out.println("The minimum transport move is: " + getMinimumTransportPort(area));
     }
-    
-    public int getMinimumTransportPort(){
-        return -1;
+
+    public int getMinimumTransportPort(List<List<Integer>> area) {
+        boolean isFirst = false;
+        boolean isFinal = false;
+        int initialValue = 1;
+        int finalValue = 0;
+
+        int numColumns = area.size();
+
+        for (List<Integer> ll : area) {
+            for (int i = 0; i < numColumns; i++) {
+                if (isFirst) {
+                    if (ll.get(i) == 1) {
+                        initialValue++;
+                    } else if (ll.get(i) == 9) {
+                        if (!isFinal) {
+                            finalValue = initialValue;
+                        }
+                        isFinal = true;
+                    }
+                }
+                isFirst = true;
+            }
+        }
+        return finalValue;
     }
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         TransportSystem transportSystem = new TransportSystem();
     }
 }
