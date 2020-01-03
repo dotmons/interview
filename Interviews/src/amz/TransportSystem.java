29c8c55201da1f846ce96c592ee992c968415153
Adeoye Oludotun <dotmons@gmail.com>
2020-01-02 22:24:21 -0500

    Adding code base: Transport System

--- Interviews/src/amz/TransportSystem.java
+++ Interviews/src/amz/TransportSystem.java
@@ -13,7 +13,7 @@ import java.util.List;
  *
  * @author Dotmons
  */
-public class TransportSystem {
+public final class TransportSystem {
 
     public TransportSystem() {
 
@@ -32,8 +32,6 @@ public class TransportSystem {
             for (int j = 0; j < dp[i].length; j++) {
                 dp[i][j] = area.get(i).get(j);
 
-                int temp = dp[i][j];
-
                 if (dp[i][j] == 9) {
 
                     if (i > 0 && j > 0) {
@@ -49,51 +47,18 @@ public class TransportSystem {
                         int tempValue = (dp[i - 1][j] > 0) ? dp[i - 1][j] : dp[i][j - 1];
                         dp[i][j] += tempValue;
                     }
-                    //System.out.println("Value:: " + dp[i][j]);
-                    //System.out.println("IIJJ: " + temp + "  dp[i][j]: " + dp[i][j] + ";  i: " + i + ";  j: = " + j);
                 } else if (i > 0) {
-
                     if ((dp[i][j] > 0) && (dp[i - 1][j] > 0)) {
                         dp[i][j] += dp[i - 1][j];
                     }
-                    //System.out.println("IIII: " + temp + "  dp[i][j]: " + dp[i][j] + ";  i: " + i + ";  j: = " + j);
                 } else if (j > 0) {
                     if ((dp[i][j] == 1) && (dp[i][j - 1] == 1)) {
                         dp[i][j] += dp[i][j - 1];
                     }
-                    //System.out.println("JJJJ: " + temp + "  dp[i][j]: " + dp[i][j] + ";  i: " + i + ";  j: = " + j);
-                } else {
-                    //System.out.println("NULL: " + temp + "  dp[i][j]: " + dp[i][j] + ";  i: " + i + ";  j: = " + j);
                 }
             }
         }
         return finalDestination;
-    }
-
-    public int getMinimumTransportPort(List<List<Integer>> area) {
-        boolean isFirst = false;
-        boolean isFinal = false;
-        int initialValue = 1;
-        int finalValue = 0;
-
-        int numColumns = area.size();
-
-        for (List<Integer> ll : area) {
-            for (int i = 0; i < numColumns; i++) {
-                if (isFirst) {
-                    if (ll.get(i) == 1) {
-                        initialValue++;
-                    } else if (ll.get(i) == 9) {
-                        if (!isFinal) {
-                            finalValue = initialValue;
-                        }
-                        isFinal = true;
-                    }
-                }
-                isFirst = true;
-            }
-        }
-        return finalValue;
     }
 
     public List<List<Integer>> testCaseA() {
@@ -141,14 +106,14 @@ public class TransportSystem {
         listValues.add(0);
         listValues.add(1);
         area.add(listValues);
-        
+
         listValues = new ArrayList();
         listValues.add(1);
         listValues.add(1);
         listValues.add(9);
         listValues.add(1);
         area.add(listValues);
-        
+
         listValues = new ArrayList();
         listValues.add(0);
         listValues.add(0);
