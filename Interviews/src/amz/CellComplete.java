ac54f87d3c0d7519ea0941a7d94afe5d06141058
dotmons <dotmons@gmail.com>
2024-07-16 09:24:55 -0400

    Unsolved solution

--- Interviews/src/amz/CellComplete.java
+++ Interviews/src/amz/CellComplete.java
@@ -5,11 +5,25 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/*
+  There is a colony of 8 cells arranged in a straight line where each day every cell competes with its adjacent cells(neighbour).
+  Each day, for each cell, if its neighbours are both active or both inactive, the cell becomes inactive the next day,.
+  otherwise it becomes active the next day.
+  Assumptions: The two cells on the ends have single adjacent cell, so the other adjacent cell can be assumed to be always
+  inactive. Even after updating the cell state. consider its previous state for updating the state of other cells.
+  Update the cell information of all cells simultaneously.
+  Write a function cellCompete which takes one 8 element array of integers cells representing the current state of
+  8 cells and one integer days representing te number of days to simulate. An integer value of 1 represents an active cell
+  and value of 0 represents an inactive cell.
+*/
+
 public class CellComplete {
 
     public CellComplete() {
-        int[] lValue = {1, 1, 1, 0, 1, 1, 1, 1};
-        int days = 2;
+        int[] lValue = {1, 0, 0, 0, 0, 1, 0, 0};
+        // 1,0,0,1,0,0,0,0
+        //
+        int days = 1;
         System.out.println(cellCompete(lValue, days));
 
     }
